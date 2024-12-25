33422d678cfae4075c7fced016e224d94b3726ef
Tejas <tejassj1998@gmail.com>
2024-12-25 22:14:59 +0530

    new api added

--- EntityFrameworkUse1.0.0/Controllers/EmployeeController.cs
+++ EntityFrameworkUse1.0.0/Controllers/EmployeeController.cs
@@ -6,13 +6,18 @@ namespace EntityFrameworkUse1._0._0.Controllers
 {
     public class EmployeeController : Controller
     {
+        #region Object Declaration
         private readonly ApplicationContext _context;
-        
+        #endregion
+
+        #region Object Assigning
         public EmployeeController(ApplicationContext context)
         {
             _context = context;
         }
+        #endregion
 
+        #region Use of Get
         [HttpGet]
         [Route("Employees")]
         public IActionResult GetEmployees()
@@ -110,7 +115,9 @@ namespace EntityFrameworkUse1._0._0.Controllers
                         }).ToList();
             return Json(data);
         }
+#endregion
 
+        #region Use of Post
         [HttpPost]
         [Route("HR/AddManager")]
         public IActionResult AddManager([FromForm] String M_F_Name, [FromForm] String M_L_Name,[FromForm] String M_Address)
@@ -222,5 +229,113 @@ namespace EntityFrameworkUse1._0._0.Controllers
                 return StatusCode(500,new { message="Internal Server Error"});
             }
         }
+        #endregion
+
+        #region Use of Put
+        [HttpPut]
+        [Route("Hr/EmployeeUpdate")]
+        public IActionResult UpdateEmployee ([FromForm] Int64 E_Id, [FromForm] Int64 M_Id, [FromForm] String E_F_Name, [FromForm] String E_L_Name, [FromForm] Double Salary, [FromForm] String E_Address, [FromForm] string E_MobileNo, [FromForm] string E_Email)
+        {
+            try
+            {
+                if ((E_Id==0) || (M_Id == 0) || String.IsNullOrEmpty(E_F_Name.Trim()) || String.IsNullOrEmpty(E_L_Name.Trim()) || (Salary == 0))
+                {
+                    return BadRequest(new { message = "All Fields are required" });
+                }
+                var employee = _context.Employees.FirstOrDefault(e=>e.E_Id==E_Id);
+                if(employee==null)
+                {
+                    return StatusCode(404,new { message = "Employee Following Id Not Found" });
+                }
+                var existing1 = _context.Employees.Where(s=>s.E_Id==E_Id).FirstOrDefault<Employee>();
+                var existing2 = _context.EployeeDetails.Where(s => s.E_Id == E_Id).FirstOrDefault<EployeeDetails>();
+
+                if (existing1 != null) 
+                {
+                    existing1.E_F_Name = E_F_Name;
+                    existing1.E_L_Name = E_L_Name;
+                    existing1.Salary = Salary;
+                    existing1.M_Id = M_Id;
+                }
+                
+                if (existing2 != null) 
+                {
+                    existing2.Email = E_Email;
+                    existing2.MobileNo = E_MobileNo;
+                    existing2.Address = E_Address;
+                }
+                _context.SaveChanges();
+
+                return Ok(new { message = "Employee Updated" });
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new {message="Internal Server Error" });
+            }
+        }
+
+        [HttpPut]
+        [Route("Hr/ManagerUpdate")]
+        public IActionResult UpdateManager([FromForm] Int64 M_Id, [FromForm] String M_F_Name, [FromForm] String M_L_Name, [FromForm] string Address)
+        {
+            try
+            {
+                if ( (M_Id == 0) || String.IsNullOrEmpty(M_F_Name.Trim()) || String.IsNullOrEmpty(M_L_Name.Trim()) || String.IsNullOrEmpty(Address.Trim()))
+                {
+                    return BadRequest(new { message = "All Fields are required" });
+                }
+                var existing = _context.Managers.Where(m => m.M_Id == M_Id).FirstOrDefault<Manager>();
+                if(existing!=null)
+                {
+                    existing.M_F_Name = M_F_Name;
+                    existing.M_L_Name = M_L_Name;
+                    existing.M_Address = Address;
+                    _context.SaveChanges();
+                    return Ok(new {message="Manager Updated"});
+                }
+                else
+                {
+                    return StatusCode(404, new { message = "Manager With Id Not Found" });
+                }
+            }
+            catch (Exception)
+            {
+               return StatusCode(500,new{ message="Internal Server Error"});
+            }
+        }
+
+
+        [HttpPut]
+        [Route("Manager/UpdateEmployeeProject")]
+        public IActionResult UpdateEmployeeProject([FromForm] Int64 E_Id, [FromForm] Int64 M_Id)
+        {
+            try
+            {
+                if ((E_Id==0)||(M_Id==0))
+                {
+                    return BadRequest(new {message="All Fields are Required" });
+                }
+                var data = _context.EmployeeProjects.Where(e => e.E_Id == E_Id).FirstOrDefault<EmployeeProject>();
+                if(data!=null)
+                {
+                    data.Projects_Id = M_Id;
+                    _context.SaveChanges();
+                    return Ok(new { message = "Employee assigned to the Project" });
+                }
+                else
+                {
+                    return StatusCode(404,new { message="Employee not found to assign the Project"});
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500,new { message="Internal Server Error"});
+            }
+        }
+
+        #endregion
+
+        #region Use of Delete
+        #endregion
     }
 }
