509f610cf2ecf4292ea2cb75a0e9dd26a307672b
Tejas <tejassj1998@gmail.com>
2024-12-26 13:12:58 +0530

    delete api added

--- EntityFrameworkUse1.0.0/Controllers/EmployeeController.cs
+++ EntityFrameworkUse1.0.0/Controllers/EmployeeController.cs
@@ -1,7 +1,6 @@
 ﻿using EntityFrameworkUse1._0._0.Data;
 using EntityFrameworkUse1._0._0.module;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 namespace EntityFrameworkUse1._0._0.Controllers
 {
     public class EmployeeController : Controller
@@ -25,7 +24,7 @@ namespace EntityFrameworkUse1._0._0.Controllers
             var employees = (from e in _context.Employees
                              join ed in _context.EployeeDetails
                              on e.E_Id equals ed.E_Id
-                             join m in _context.Managers 
+                             join m in _context.Managers
                              on e.M_Id equals m.M_Id
                              select new
                              {
@@ -47,25 +46,25 @@ namespace EntityFrameworkUse1._0._0.Controllers
         public IActionResult GetEmployee(Int64 id)
         {
             Console.WriteLine(id);
-            var data=(from e in _context.Employees
-                            join ed in _context.EployeeDetails
-                            on e.E_Id equals ed.E_Id
-                            join m in _context.Managers
-                            on e.M_Id equals m.M_Id
-                            where e.E_Id == id
-                            select new
-                            {
-                                e.E_Id,
-                                e.E_F_Name,
-                                e.E_L_Name,
-                                e.Salary,
-                                ed.Email,
-                                ed.MobileNo,
-                                ed.Address,
-                                m.M_F_Name,
-                                m.M_L_Name
-                            }).ToList();
-                return Json(data);
+            var data = (from e in _context.Employees
+                        join ed in _context.EployeeDetails
+                        on e.E_Id equals ed.E_Id
+                        join m in _context.Managers
+                        on e.M_Id equals m.M_Id
+                        where e.E_Id == id
+                        select new
+                        {
+                            e.E_Id,
+                            e.E_F_Name,
+                            e.E_L_Name,
+                            e.Salary,
+                            ed.Email,
+                            ed.MobileNo,
+                            ed.Address,
+                            m.M_F_Name,
+                            m.M_L_Name
+                        }).ToList();
+            return Json(data);
         }
 
         [HttpGet]
@@ -74,11 +73,11 @@ namespace EntityFrameworkUse1._0._0.Controllers
         {
             var Managers = (from m in _context.Managers
                             select new
-                            { 
-                               m.M_Id,
-                               m.M_F_Name,
-                               m.M_L_Name,
-                               m.M_Address
+                            {
+                                m.M_Id,
+                                m.M_F_Name,
+                                m.M_L_Name,
+                                m.M_Address
                             }).ToList();
             return Json(Managers);
         }
@@ -87,12 +86,12 @@ namespace EntityFrameworkUse1._0._0.Controllers
         [Route("Projects")]
         public IActionResult GetProjects()
         {
-            var Projects =(from p in _context.Projects
-                           select new
-                           {
-                               p.Project_Id,
-                               p.PrjectName
-                           }).ToList();
+            var Projects = (from p in _context.Projects
+                            select new
+                            {
+                                p.Project_Id,
+                                p.PrjectName
+                            }).ToList();
             return Json(Projects);
         }
 
@@ -115,60 +114,61 @@ namespace EntityFrameworkUse1._0._0.Controllers
                         }).ToList();
             return Json(data);
         }
-#endregion
+        #endregion
 
         #region Use of Post
         [HttpPost]
         [Route("HR/AddManager")]
-        public IActionResult AddManager([FromForm] String M_F_Name, [FromForm] String M_L_Name,[FromForm] String M_Address)
+        public IActionResult AddManager([FromForm] String M_F_Name, [FromForm] String M_L_Name, [FromForm] String M_Address)
         {
             try
             {
-                if (String.IsNullOrEmpty(M_F_Name.Trim()) || String.IsNullOrEmpty(M_L_Name.Trim()) || String.IsNullOrEmpty(M_Address.Trim())) 
+                if (String.IsNullOrEmpty(M_F_Name.Trim()) || String.IsNullOrEmpty(M_L_Name.Trim()) || String.IsNullOrEmpty(M_Address.Trim()))
                 {
-                    return BadRequest(new {message="All Fields are required"});
+                    return BadRequest(new { message = "All Fields are required" });
                 }
-                var manager = new Manager {
+                var manager = new Manager
+                {
                     M_F_Name = M_F_Name.Trim(),
                     M_L_Name = M_L_Name.Trim(),
                     M_Address = M_Address.Trim(),
                 };
                 _context.Managers.Add(manager);
                 _context.SaveChanges();
-                
-                return Ok(new {message="Manager Added"});
+
+                return Ok(new { message = "Manager Added" });
             }
             catch (Exception ex)
             {
-                return StatusCode(500,new {message="Internal Server Error"});
+                return StatusCode(500, new { message = "Internal Server Error" });
             }
         }
 
         [HttpPost]
         [Route("HR/AddEmployee")]
-        public IActionResult AddEmployee([FromForm] Int64 M_Id,[FromForm] String E_F_Name, [FromForm] String E_L_Name, [FromForm] Double Salary, [FromForm] String E_Address, [FromForm] string E_MobileNo, [FromForm] string E_Email)
+        public IActionResult AddEmployee([FromForm] Int64 M_Id, [FromForm] String E_F_Name, [FromForm] String E_L_Name, [FromForm] Double Salary, [FromForm] String E_Address, [FromForm] string E_MobileNo, [FromForm] string E_Email)
         {
             try
             {
-                if ((M_Id==0)|| String.IsNullOrEmpty(E_F_Name.Trim()) || String.IsNullOrEmpty(E_L_Name.Trim()) || (Salary==0))
+                if ((M_Id == 0) || String.IsNullOrEmpty(E_F_Name.Trim()) || String.IsNullOrEmpty(E_L_Name.Trim()) || (Salary == 0))
                 {
                     return BadRequest(new { message = "All Fields are required" });
                 }
                 var employee = new Employee
                 {
-                    M_Id=M_Id,
-                    E_F_Name=E_F_Name.Trim(),
-                    E_L_Name=E_L_Name.Trim(),
-                    Salary=Salary,
+                    M_Id = M_Id,
+                    E_F_Name = E_F_Name.Trim(),
+                    E_L_Name = E_L_Name.Trim(),
+                    Salary = Salary,
                 };
                 _context.Employees.Add(employee);
                 _context.SaveChanges();
                 var employeeDetails = new EployeeDetails
                 {
-                    E_Id=employee.E_Id,
-                    MobileNo=E_MobileNo.Trim(),
-                    Address=E_Address.Trim(),
-                    Email=E_Email.Trim(),
+                    E_Id = employee.E_Id,
+                    MobileNo = E_MobileNo.Trim(),
+                    Address = E_Address.Trim(),
+                    Email = E_Email.Trim(),
                 };
                 _context.EployeeDetails.Add(employeeDetails);
                 _context.SaveChanges();
@@ -201,13 +201,13 @@ namespace EntityFrameworkUse1._0._0.Controllers
             }
             catch (Exception)
             {
-                return StatusCode(500,new {message="Internal Server Error"});
+                return StatusCode(500, new { message = "Internal Server Error" });
             }
         }
 
         [HttpPost]
         [Route("Manager/AssignProjectToEmployee")]
-        public IActionResult AssignProjectToEmployee([FromForm] Int64 E_Id, [FromForm] Int64 Project_Id )
+        public IActionResult AssignProjectToEmployee([FromForm] Int64 E_Id, [FromForm] Int64 Project_Id)
         {
             try
             {
@@ -222,11 +222,11 @@ namespace EntityFrameworkUse1._0._0.Controllers
                 };
                 _context.EmployeeProjects.Add(employeeProject);
                 _context.SaveChanges();
-                return Ok( new {message="Project Assigne to employee" });
+                return Ok(new { message = "Project Assigne to employee" });
             }
             catch (Exception)
             {
-                return StatusCode(500,new { message="Internal Server Error"});
+                return StatusCode(500, new { message = "Internal Server Error" });
             }
         }
         #endregion
@@ -234,31 +234,31 @@ namespace EntityFrameworkUse1._0._0.Controllers
         #region Use of Put
         [HttpPut]
         [Route("Hr/EmployeeUpdate")]
-        public IActionResult UpdateEmployee ([FromForm] Int64 E_Id, [FromForm] Int64 M_Id, [FromForm] String E_F_Name, [FromForm] String E_L_Name, [FromForm] Double Salary, [FromForm] String E_Address, [FromForm] string E_MobileNo, [FromForm] string E_Email)
+        public IActionResult UpdateEmployee([FromForm] Int64 E_Id, [FromForm] Int64 M_Id, [FromForm] String E_F_Name, [FromForm] String E_L_Name, [FromForm] Double Salary, [FromForm] String E_Address, [FromForm] string E_MobileNo, [FromForm] string E_Email)
         {
             try
             {
-                if ((E_Id==0) || (M_Id == 0) || String.IsNullOrEmpty(E_F_Name.Trim()) || String.IsNullOrEmpty(E_L_Name.Trim()) || (Salary == 0))
+                if ((E_Id == 0) || (M_Id == 0) || String.IsNullOrEmpty(E_F_Name.Trim()) || String.IsNullOrEmpty(E_L_Name.Trim()) || (Salary == 0))
                 {
                     return BadRequest(new { message = "All Fields are required" });
                 }
-                var employee = _context.Employees.FirstOrDefault(e=>e.E_Id==E_Id);
-                if(employee==null)
+                var employee = _context.Employees.FirstOrDefault(e => e.E_Id == E_Id);
+                if (employee == null)
                 {
-                    return StatusCode(404,new { message = "Employee Following Id Not Found" });
+                    return StatusCode(404, new { message = "Employee Following Id Not Found" });
                 }
-                var existing1 = _context.Employees.Where(s=>s.E_Id==E_Id).FirstOrDefault<Employee>();
+                var existing1 = _context.Employees.Where(s => s.E_Id == E_Id).FirstOrDefault<Employee>();
                 var existing2 = _context.EployeeDetails.Where(s => s.E_Id == E_Id).FirstOrDefault<EployeeDetails>();
 
-                if (existing1 != null) 
+                if (existing1 != null)
                 {
                     existing1.E_F_Name = E_F_Name;
                     existing1.E_L_Name = E_L_Name;
                     existing1.Salary = Salary;
                     existing1.M_Id = M_Id;
                 }
-                
-                if (existing2 != null) 
+
+                if (existing2 != null)
                 {
                     existing2.Email = E_Email;
                     existing2.MobileNo = E_MobileNo;
@@ -270,7 +270,7 @@ namespace EntityFrameworkUse1._0._0.Controllers
             }
             catch (Exception)
             {
-                return StatusCode(500, new {message="Internal Server Error" });
+                return StatusCode(500, new { message = "Internal Server Error" });
             }
         }
 
@@ -280,18 +280,18 @@ namespace EntityFrameworkUse1._0._0.Controllers
         {
             try
             {
-                if ( (M_Id == 0) || String.IsNullOrEmpty(M_F_Name.Trim()) || String.IsNullOrEmpty(M_L_Name.Trim()) || String.IsNullOrEmpty(Address.Trim()))
+                if ((M_Id == 0) || String.IsNullOrEmpty(M_F_Name.Trim()) || String.IsNullOrEmpty(M_L_Name.Trim()) || String.IsNullOrEmpty(Address.Trim()))
                 {
                     return BadRequest(new { message = "All Fields are required" });
                 }
                 var existing = _context.Managers.Where(m => m.M_Id == M_Id).FirstOrDefault<Manager>();
-                if(existing!=null)
+                if (existing != null)
                 {
                     existing.M_F_Name = M_F_Name;
                     existing.M_L_Name = M_L_Name;
                     existing.M_Address = Address;
                     _context.SaveChanges();
-                    return Ok(new {message="Manager Updated"});
+                    return Ok(new { message = "Manager Updated" });
                 }
                 else
                 {
@@ -300,7 +300,7 @@ namespace EntityFrameworkUse1._0._0.Controllers
             }
             catch (Exception)
             {
-               return StatusCode(500,new{ message="Internal Server Error"});
+                return StatusCode(500, new { message = "Internal Server Error" });
             }
         }
 
@@ -311,12 +311,12 @@ namespace EntityFrameworkUse1._0._0.Controllers
         {
             try
             {
-                if ((E_Id==0)||(M_Id==0))
+                if ((E_Id == 0) || (M_Id == 0))
                 {
-                    return BadRequest(new {message="All Fields are Required" });
+                    return BadRequest(new { message = "All Fields are Required" });
                 }
                 var data = _context.EmployeeProjects.Where(e => e.E_Id == E_Id).FirstOrDefault<EmployeeProject>();
-                if(data!=null)
+                if (data != null)
                 {
                     data.Projects_Id = M_Id;
                     _context.SaveChanges();
@@ -324,18 +324,110 @@ namespace EntityFrameworkUse1._0._0.Controllers
                 }
                 else
                 {
-                    return StatusCode(404,new { message="Employee not found to assign the Project"});
+                    return StatusCode(404, new { message = "Employee not found to assign the Project" });
                 }
             }
             catch (Exception)
             {
-                return StatusCode(500,new { message="Internal Server Error"});
+                return StatusCode(500, new { message = "Internal Server Error" });
             }
         }
 
         #endregion
 
         #region Use of Delete
+        [HttpDelete]
+        [Route("Hr/DeleteEmployee")]
+        public IActionResult DeleteEmployee([FromForm] Int64 E_Id)
+        {
+            try
+            {
+                if (E_Id == 0)
+                {
+                    return BadRequest(new { message = $"Employee Id is required{E_Id}" });
+                }
+                var employeeDetails = (from e in _context.Employees
+                                       join ed in _context.EployeeDetails
+                                       on e.E_Id equals ed.E_Id
+                                       where e.E_Id == E_Id
+                                       select new
+                                       {
+                                           Employees = e,
+                                           EployeeDetails = ed
+                                       }).FirstOrDefault();
+                if (employeeDetails == null)
+                {
+                    return StatusCode(404, new { message = $"Employee with  id= {E_Id} Not Found" });
+                }
+                else
+                {
+                    _context.Employees.Remove(employeeDetails.Employees);
+                    _context.EployeeDetails.Remove(employeeDetails.EployeeDetails);
+                    _context.SaveChanges();
+                    return Ok(new { message = "Employee is Removed" });
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new { message = "Internal Server Error" });
+            }
+        }
+
+        [HttpDelete]
+        [Route("Hr/DeleteManager")]
+        public IActionResult DeleteManager([FromForm] Int64 M_Id)
+        {
+            try
+            {
+                if (M_Id == 0)
+                {
+                    return BadRequest(new { message = $"Employee Id is required{M_Id}" });
+                }
+                var managerDetails = _context.Managers.Where(m => m.M_Id == M_Id).FirstOrDefault<Manager>();
+                if (managerDetails == null)
+                {
+                    return StatusCode(404, new { message = $"Manager Id is required{M_Id}" });
+                }
+                else
+                {
+                    _context.Managers.Remove(managerDetails);
+                    _context.SaveChanges();
+                    return Ok(new { message = "Manager is Removed" });
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new { message = "Internal Server Error" });
+            }
+        }
+
+        [HttpDelete]
+        [Route("Manager/RemoveEmployeeProject")]
+        public IActionResult RemoveEmployeeProject([FromForm] Int64 E_Id)
+        {
+            try
+            {
+                if(E_Id==0)
+                {
+                    return BadRequest(new{message="Employee Id is Required" });
+                }
+                var employeeProject = _context.EmployeeProjects.Where(ep=>ep.E_Id==E_Id).FirstOrDefault<EmployeeProject>();
+                if(employeeProject==null)
+                {
+                    return StatusCode(404, new { message = $"Employee Id= {E_Id} Not Found" });
+                }
+                else
+                {
+                    _context.EmployeeProjects.Remove(employeeProject);
+                    _context.SaveChanges();
+                    return Ok(new {message="Project Removed from the Employee" });
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new { message = "Internal Server Error" });
+            }
+        }
         #endregion
     }
 }
